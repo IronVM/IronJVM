b9165b1080bd8d1124b54d3b14b11afcae3a1b25
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-03-30 20:32:37 +0800

    add classfile basic impl

---
+++ ironjvm_specimpl/src/classfile/flags.rs
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * IronJVM: JVM Implementation in Rust
+ * Copyright (C) 2022 HTGAzureX1212.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+pub struct ClassAccessFlags;
+
+impl ClassAccessFlags {
+    pub const ACC_PUBLIC: u16 = 0x0001;
+    pub const ACC_FINAL: u16 = 0x0010;
+    pub const ACC_SUPER: u16 = 0x0020;
+    pub const ACC_INTERFACE: u16 = 0x0200;
+    pub const ACC_ABSTRACT: u16 = 0x0400;
+    pub const ACC_SYNTHETIC: u16 = 0x1000;
+    pub const ACC_ANNOTATION: u16 = 0x2000;
+    pub const ACC_ENUM: u16 = 0x4000;
+    pub const ACC_MODULE: u16 = 0x8000;
+}
+
+pub struct FieldAccessFlags;
+
+impl FieldAccessFlags {
+    pub const ACC_PUBLIC: u16 = 0x0001;
+    pub const ACC_PRIVATE: u16 = 0x0002;
+    pub const ACC_PROTECTED: u16 = 0x0004;
+    pub const ACC_STATIC: u16 = 0x0008;
+    pub const ACC_FINAL: u16 = 0x0010;
+    pub const ACC_VOLATILE: u16 = 0x0040;
+    pub const ACC_TRANSIENT: u16 = 0x0080;
+    pub const ACC_SYNTHETIC: u16 = 0x1000;
+    pub const ACC_ENUM: u16 = 0x4000;
+}
+
+pub struct MethodAccessFlags;
+
+impl MethodAccessFlags {
+    pub const ACC_PUBLIC: u16 = 0x0001;
+    pub const ACC_PRIVATE: u16 = 0x0002;
+    pub const ACC_PROTECTED: u16 = 0x0004;
+    pub const ACC_STATIC: u16 = 0x0008;
+    pub const ACC_FINAL: u16 = 0x0010;
+    pub const ACC_SYNCHRONIZED: u16 = 0x0020;
+    pub const ACC_BRIDGE: u16 = 0x0040;
+    pub const ACC_VARARGS: u16 = 0x0080;
+    pub const ACC_NATIVE: u16 = 0x0100;
+    pub const ACC_ABSTRACT: u16 = 0x0400;
+    pub const ACC_STRICT: u16 = 0x0800;
+    pub const ACC_SYNTHETIC: u16 = 0x1000;
+}
---
+++ ironjvm_specimpl/src/classfile/mod.rs
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * IronJVM: JVM Implementation in Rust
+ * Copyright (C) 2022 HTGAzureX1212.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+pub mod flags;
+
+#[repr(C)]
+pub struct AttributeInfo {
+    pub attribute_name_index: u16,
+    pub attribute_length: u32,
+    pub info: Vec<u8>,
+}
+
+#[repr(C)]
+pub struct ClassFile {
+    pub magic: u32,
+    pub minor_version: u16,
+    pub major_version: u16,
+    pub constant_pool_count: u16,
+    pub constant_pool: Vec<CpInfo>,
+    pub access_flags: u16,
+    pub this_class: u16,
+    pub super_class: u16,
+    pub interfaces_count: u16,
+    pub interfaces: Vec<u16>,
+    pub fields_count: u16,
+    pub fields: Vec<FieldInfo>,
+    pub methods_count: u16,
+    pub methods: Vec<MethodInfo>,
+    pub attributes_count: u16,
+    pub attributes: Vec<AttributeInfo>,
+}
+
+#[repr(C)]
+pub struct CpInfo {
+    pub tag: u8,
+    pub info: Vec<u8>,
+}
+
+#[repr(C)]
+pub struct FieldInfo {
+    pub access_flags: u16,
+    pub name_index: u16,
+    pub descriptor_index: u16,
+    pub attributes_count: u16,
+    pub attributes: Vec<AttributeInfo>,
+}
+
+#[repr(C)]
+pub struct MethodInfo {
+    pub access_flags: u16,
+    pub name_index: u16,
+    pub descriptor_index: u16,
+    pub attributes_count: u16,
+    pub attributes: Vec<AttributeInfo>,
+}
